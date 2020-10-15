534af74fac426c1de008a5671981b6fc3f46decf
Lindsey Fund <fun20001@byui.edu>
2020-10-14 19:20:53 -0700

    first push lesson 5

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+}
+
+const options = {weekday:'long',  day:'numeric',  month:"long", year: 'numeric'};
+    document.getElementById('CurrentDate').textContent = new Date().toLocaleDateString('en-US', options);
