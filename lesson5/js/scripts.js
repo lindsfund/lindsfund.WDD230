f1fd44b1ced653b0064cc0961e71ff8952101ff6
Lindsey Fund <fun20001@byui.edu>
2020-10-17 13:40:55 -0700

    banner trial

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -5,3 +5,14 @@ function toggleMenu() {
 
 const options = {weekday:'long',  day:'numeric',  month:"long", year: 'numeric'};
     document.getElementById('CurrentDate').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+let d = new Date();
+
+d.getDay();
+
+if (d.getDay() == 5) {
+    document.querySelector('.condBanner').getElementsByClassName.display = "block";
+} else {
+    document.querySelector('.condBanner').getElementsByClassName.display = "none";
+}
