b303d407efdb19f3cf177a5ebdc92e8f4a405710
Lindsey Fund <fun20001@byui.edu>
2020-10-17 15:58:46 -0700

    banner added

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -7,12 +7,3 @@ const options = {weekday:'long',  day:'numeric',  month:"long", year: 'numeric'}
     document.getElementById('CurrentDate').textContent = new Date().toLocaleDateString('en-US', options);
 
 
-let d = new Date();
-
-d.getDay();
-
-if (d.getDay() == 5) {
-    document.querySelector('.condBanner').getElementsByClassName.display = "block";
-} else {
-    document.querySelector('.condBanner').getElementsByClassName.display = "none";
-}
