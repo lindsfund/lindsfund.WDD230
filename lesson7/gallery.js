271358e06ed6436c1234698b47eb6a76b9f1390d
Lindsey Fund <fun20001@byui.edu>
2020-10-28 10:18:31 -0700

    image gallery

---
+++ lesson7/gallery.js
@@ -0,0 +1,38 @@
+
+//load all images with a data src attribute
+const photo = document.querySelectorAll("img[data-src]");
+
+//settings for the InstersectionObserver
+const options = {
+    threshold: 0,
+    rootMargin: "0px 0px 200px 0px"
+  };
+
+
+
+  const loadImages = (image) => {
+      image.setAttribute("src", image.getAttribute("data-src"));
+      image. onload = () => {
+          image.removeAttribute("data-src");
+      };
+  };
+
+
+//is the image intersecting the Viewport (IntersectionObserver)
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return;
+    }else{
+      console.log(entry.target);
+      loadImages(entry.target);
+      observer.unobserve(entry.target);
+    }
+    
+  });
+}, options);
+
+//loops through each img to check and load 
+photo.forEach((img) => {
+        observer.observe(img);
+    });
