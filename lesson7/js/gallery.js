2556bb7f258bb1104a5c0bba2fc603290346a3de
Lindsey Fund <fun20001@byui.edu>
2020-10-30 10:50:48 -0700

    fixed js and validated

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -3,7 +3,7 @@
 const photo = document.querySelectorAll("img[data-src]");
 
 //settings for the InstersectionObserver
-const options = {
+const imgOptions = {
     threshold: 0,
     rootMargin: "0px 0px 200px 0px"
   };
@@ -29,7 +29,7 @@ const observer = new IntersectionObserver(function(entries, observer) {
     }
     
   });
-}, options);
+}, imgOptions);
 
 //loops through each img to check and load 
 photo.forEach((img) => {
