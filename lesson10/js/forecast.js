3429c5e5ac15ba846e9db520ae88248149c1be17
Lindsey Fund <fun20001@byui.edu>
2020-11-20 11:38:38 -0700

    It works! but it's not perty yet

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -5,7 +5,7 @@ fetch (apiURL)
     .then(jsObject => {
 
         const forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-        console.log(forecast);
+        // console.log(forecast);
 
         const weekdays = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
         
@@ -13,15 +13,19 @@ fetch (apiURL)
             const d = new Date(forecast[day].dt_txt);
     
             const wDay = weekdays[d.getDay()];
-            console.log(wDay);
+            // console.log(wDay);
 
             let kelvin = forecast[day].main.temp;
             let celsius = kelvin - 273;
             let fahrenheit = Math.floor(celsius * (9 / 5) + 32);
             
+            const imagesrc = "https://openweathermap.org/img/w/" + forecast[day].weather[0].icon + ".png";
+
             document.getElementById(`forecast${day+1}`).textContent = fahrenheit;
             document.getElementById(`dayofweek${day+1}`).textContent = wDay;
-            
+            document.getElementById(`imgIcon${day+1}`).setAttribute("src", imagesrc);
+            document.getElementById(`imgIcon${day+1}`).setAttribute("alt", forecast[day].weather[0].main);
+
         }
 
     });
