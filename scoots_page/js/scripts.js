bf80720596f772f975d329c108f4a999f27c8aec
lindsfund <fun20001@byui.edu>
2020-12-03 15:23:06 -0700

    nav fixes

--- scoots_page/js/scripts.js
+++ scoots_page/js/scripts.js
@@ -1,3 +1,6 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+const options = {weekday:'long',  day:'numeric',  month:"long", year: 'numeric'};
+    document.getElementById('CurrentDate').textContent = new Date().toLocaleDateString('en-US', options);
