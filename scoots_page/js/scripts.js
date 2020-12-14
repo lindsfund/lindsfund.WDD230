dc65c831aa8e79a0c731974174291b93e1cc1c70
lindsfund <fun20001@byui.edu>
2020-12-14 11:27:27 -0700

    Last updated js

--- scoots_page/js/scripts.js
+++ scoots_page/js/scripts.js
@@ -2,5 +2,7 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+document.getElementById("LastModified").innerHTML = document.lastModified;
+
 const options = {weekday:'long',  day:'numeric',  month:"long", year: 'numeric'};
     document.getElementById('CurrentDate').textContent = new Date().toLocaleDateString('en-US', options);
